8d91417ce771a723586ba29b8a63449e4a0f8a03
bhogesh4i9 <vurabhogesh@gmail.com>
2025-07-15 01:35:38 -0400

    firebase config is stored in env

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -5,14 +5,15 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore"; 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCrSsLDrgPljlypyva2Vpbgej52xYz67uE",
-  authDomain: "live-location-tracker-cdb98.firebaseapp.com",
-  projectId: "live-location-tracker-cdb98",
-  storageBucket: "live-location-tracker-cdb98.appspot.com", 
-  messagingSenderId: "866669048420",
-  appId: "1:866669048420:web:351071b6eb88369977e571",
-  measurementId: "G-QTYCQSMDKQ"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
