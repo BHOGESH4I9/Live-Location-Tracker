a1ed014e0a07232fa01c9e2ac252b6206e5c6872
bhogesh4i9 <vurabhogesh@gmail.com>
2025-07-15 01:34:38 -0400

    admin users are stored in env

--- src/adminlogin/adminUsers.js
+++ src/adminlogin/adminUsers.js
@@ -1,4 +1,1 @@
-export const adminUsers = [
-  { username: "Rahul", email: 'admin1@pranathi.com', password: 'adminpass1' },
-  { username: "Ravi", email: 'admin2@pranathi.com', password: 'adminpass2' }
-];
+export const adminUsers = JSON.parse(process.env.REACT_APP_ADMIN_USERS || "[]");
